ce55395499e1c94c379e89a9b9aef60240512dfa
Yuvi <singhraj0461@gmail.com>
2023-12-27 16:35:18 +0530

    all basic need has completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hii this  is yuvraj Singh</h1>
+      <main>
+        <Outlet />
+      </main>
+
     </>
   )
 }
